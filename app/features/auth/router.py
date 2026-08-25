399afada53efdf164f5cfb8da76ad5bff1bd80f9
Lucas <lucasdavid.bispo@gmail.com>
2026-08-25 11:07:02 -0300

    feat: rate limit for login attempts

--- app/features/auth/router.py
+++ app/features/auth/router.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.core.db.database import get_db
+from app.core.rate_limit import limiter
 
 from .dependencies import login_for_token
 from .schemas import Token
@@ -13,7 +14,11 @@ router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
 @router.post("/login", response_model=Token)
+@limiter.limit(
+    "5/minute", error_message="Too many failed login attempts.Please try again later"
+)
 async def login(
+    request: Request,
     db: Annotated[Session, Depends(get_db)],
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ):
