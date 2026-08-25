384fb07d1247f18ce6c93629e41a7f88c54ba570
Lucas <lucasdavid.bispo@gmail.com>
2026-08-24 21:10:43 -0300

    fix: exception handling for creating user and getting user by token

--- app/features/users/dependencies.py
+++ app/features/users/dependencies.py
@@ -1,6 +1,8 @@
 from typing import Annotated
 
 from fastapi import Depends, HTTPException
+from jwt.exceptions import InvalidTokenError
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from app.core.db.database import get_db
@@ -14,21 +16,37 @@ def create_user(
     user_data: UserCreate,
     db: Annotated[Session, Depends(get_db)],
 ):
-    db.add(
-        User(
-            name=user_data.name,
-            email=user_data.email,
-            password_hash=get_password_hash(user_data.password),
-            role=UserRoles.USER,
+    try:
+        db.add(
+            User(
+                name=user_data.name,
+                email=user_data.email,
+                password_hash=get_password_hash(user_data.password),
+                role=UserRoles.USER,
+            )
         )
-    )
-    db.commit()
+
+        db.commit()
+
+    except IntegrityError as exc:
+        db.rollback()
+        if exc.orig.diag.constraint_name == "users_email_key":
+            raise HTTPException(status_code=409, detail="Email already registered")
+
+        raise
+
     return UserRead(name=user_data.name, email=user_data.email)
 
 
 def get_user_by_token(db: Session, token: str):
-    user_id = decode_access_token(token)
 
+    try:
+        user_id = decode_access_token(token)
+
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=401, detail="Invalid Session. Please log in and try again."
+        )
     user_data = db.query(User).filter_by(id=int(user_id)).first()
     if user_data is None:
         raise HTTPException(status_code=404, detail="User not found")
